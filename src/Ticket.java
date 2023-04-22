67a0a06e172fae27758ea439aec6eed7c6731ac1
EhsanPazhooman <ehspazh@gmail.com>
2023-04-22 02:14:21 -0700

    add two classes for ticket option

---
+++ src/Ticket.java
@@ -0,0 +1,98 @@
+public class Ticket {
+    private String flightId ;
+    private String origin ;
+    private String destination ;
+    private String date ;
+    private String time;
+    private int price;
+    private String passengerName;
+    private int ticketId ;
+
+    public Ticket(String passengerName,String flightId,String origin, String destination, String date, String time, int price) {
+        this.passengerName = passengerName;
+        //this.ticketId = ticketId;
+        this.flightId = flightId;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+        this.time = time;
+        this.price = price;
+    }
+    //////////Passenger name getter & setter////////////////
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+    /////////// FlightId getter & setter ///////////////
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+    ////////// Origin getter & setter /////////////
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    ///////// Destination getter & setter //////////
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+    ///////// Date getter & setter //////////
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    //////////Time getter & setter ////////
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    ///////////Price getter & setter///////
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    //////////TicketId getter & setter//////
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    ////////////////////////////////
+    @Override
+    public String toString() {
+        return  "»▶ passengerName='" + passengerName + '\'' +
+                ", ticketId='" + getTicketId() + '\'' +
+                ",  flightId='" + flightId + '\'' +
+                ", origin='" + origin + '\'' +
+                ", destination='" + destination + '\'' +
+                ", date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", price=" + price ;
+    }
+}
---
+++ src/Tickets.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tickets {
+    static Scanner scanner = new Scanner(System.in);
+    static Random ticketId = new Random();
+    static String inputId;
+    static ArrayList<Ticket> boughtTickets = new ArrayList<Ticket>();
+    static Ticket[] ticketsArr = new Ticket[boughtTickets.size()];
+
+    ///////////////Method for buying ticket///////////////////////
+    public static void bookingTicket() {
+        for (int i = 0; i < Flightschedules.flights.size(); i++) {
+            System.out.println(Flightschedules.flights.get(i));
+        }
+
+        reserveFlight();
+
+        System.out.println("Your ticket has been purchased successfully");
+
+    }
+    ////////////////Method for purchased tickets/////////////////
+    public static void bookedTickets()
+    {
+        for (int i = 0; i < Login.users_arr.length; i++)
+        {
+            if (Login.users_arr[i].getUsername().equals(Login.input_username) &&
+                    Login.users_arr[i].getPassword().equals(Login.input_password)) {
+                for (int j = 0; j < Flightschedules.flights.size(); j++) {
+                    if (Flightschedules.flights.get(j).getFlightid().equals(inputId)) {
+                        Ticket ticket = new Ticket(Login.input_username,
+                                Flightschedules.flights.get(j).getFlightid(), Flightschedules.flights.get(j).getOrigin(),
+
+
+                                Flightschedules.flights.get(j).getDestination(), Flightschedules.flights.get(j).getDate(),
+                                Flightschedules.flights.get(j).getTime(), Flightschedules.flights.get(j).getPrice());
+
+                        ticket.setTicketId(ticketId.nextInt(1000, 9999));
+
+                        boughtTickets.add(ticket);
+                        ticketsArr = boughtTickets.toArray(ticketsArr);
+                        System.out.println(ticketsArr.length);
+                        System.out.println(boughtTickets.size());
+                    }
+                }
+            }
+        }
+        printTickets();
+    }
+    //////////////////Method for printing purchased tickets////////////
+    public static void printTickets(){
+        for (int i = 0; i < ticketsArr.length; i++) {
+            System.out.println(ticketsArr[i]);
+        }
+    }
+    //////////////////Method for reserving flight ticket////////////////
+    public static void reserveFlight(){
+        for (int i = 0; i < Login.users_arr.length; i++)
+        {
+            if (Login.users_arr[i].getUsername().equals(Login.input_username) &&
+                    Login.users_arr[i].getPassword().equals(Login.input_password))
+            {
+                System.out.println("Enter the flightId you want to reserve :");
+                inputId = scanner.nextLine();
+
+                for (int j = 0; j < Flightschedules.flights.size(); j++)
+                {
+                    if(Flightschedules.flights.get(j).getFlightid().equals(inputId))
+                    {
+                        if (Login.users_arr[i].getCharge() < Flightschedules.flights.get(j).getPrice()) {
+                            System.out.println("Please charge your account!");
+                            Passenger.passengerMenu();
+                        }
+
+                        else if(Flightschedules.flights.get(j).getFlightid().equals(inputId)&&Login.users_arr[i].getCharge()>=
+                                Flightschedules.flights.get(j).getPrice())
+                        {
+                            Flightschedules.flights.get(j).setSeats(Flightschedules.flights.get(j).getSeats()-1);
+                            Login.users_arr[i].setCharge(Login.users_arr[i].getCharge()-Flightschedules.flights.get(j).getPrice());
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    //////////////////Method for removing tickets////////////////////////
+    public static void removeTicket(){
+        for (int i = 0; i < Login.users.size(); i++) {
+            if(Login.users.get(i).getUsername().equals(Login.input_username)&&
+                    Login.users.get(i).getPassword().equals(Login.input_password))
+            {
+                System.out.println("Enter your ticketID");
+                String ticketId = scanner.nextLine();
+                int ticketID = Integer.parseInt(ticketId);
+                for (int j = 0; j < boughtTickets.size(); j++) {
+                    if(ticketID==boughtTickets.get(j).getTicketId())
+                    {
+                        boughtTickets.remove(j);
+
+                    }
+                }
+            }
+        }
+    }
+}
