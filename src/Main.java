f3f3cbc48678679de8912b23177a09c9b7bd48ae
EhsanPazhooman <ehspazh@gmail.com>
2023-04-22 01:41:13 -0700

    make some classes to start :)

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void pressEnterToContinue()
+    {
+        System.out.println("Press Enter key to continue...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
+}
+
+
